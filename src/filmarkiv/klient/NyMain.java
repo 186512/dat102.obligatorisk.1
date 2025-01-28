a4032643ad9a783f0d337f21c32fda4b6526d6cf
Teodor <teodor.orjansen@gmail.com>
2025-01-28 13:18:42 +0100

    h

--- src/filmarkiv/klient/NyMain.java
+++ src/filmarkiv/klient/NyMain.java
@@ -3,10 +3,15 @@ package filmarkiv.klient;
 import filmarkiv.adt.FilmarkivADT;
 import filmarkiv.impl.Filmarkiv;
 
-public class FilmarkivMain {
+public class NyMain {
+	
 	public static void main(String[] args) {
+		System.out.println("Hei");
 		FilmarkivADT filmer = new Filmarkiv(100);
 		Meny meny = new Meny(filmer);
 		meny.start();
+		System.out.println(" på deg");
+
 	}
+
 }
